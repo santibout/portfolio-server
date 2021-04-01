5de259703ff1d8ec23752fb0d9ba8a3097aa5ccf
santibout <santibout@yahoo.com>
2021-04-01 00:14:54 -0700

    removed mongo db functionality and added sendgrid

--- index.js
+++ index.js
@@ -1,54 +1,78 @@
-require("dotenv/config");
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
-
-const sendMail = require("./mail");
-const mongoose = require("mongoose");
-
+// const mongoose = require("mongoose");
 const PORT = process.env.PORT || 8080;
+const sgMail = require("@sendgrid/mail");
+const cors = require("cors");
 
-mongoose.connect("mongodb://localhost/portfolio", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-const Message = require("./models/message");
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function () {
-  // we're connected!
-  console.log("mongoose connected");
-});
+// mongoose.connect("mongodb://localhost/portfolio", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", function () {
+//   // we're connected!
+//   console.log("mongoose connected");
+// });
+
+sgMail.setApiKey(process.env.SENDGRID_ZERO_API_KEY);
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get("/", function (req, res) {
   res.send("hi");
 });
 
-app.post("/contact", function (req, res) {
+app.post("/contact", async function (req, res) {
   console.log(req.body);
   const { name, email, message } = req.body;
-  //   console.log(name, email, msg);
-  const newMessage = new Message({ name, email, message });
-  newMessage.save(function (r) {
-    if (r) {
-      console.log("error: Could not save", r);
-    } else {
-      console.log("Mail Saved to MongoDB");
+  // const newMessage = new Message({ name, email, message });
+
+  // try {
+  //   const msg = new Message({ ...newMessage });
+  //   let m = await msg.save();
+  //   console.log("M: ", m);
+  // } catch (err) {
+  //   console.log("err: ", err);
+  // }
+
+  // newMessage.save(function (r) {
+  //   if (r) {
+  //     console.log("error: Could not save", r);
+  //   } else {
+  //     console.log("Mail Saved to MongoDB");
+  //   }
+  // });
+  const msg = {
+    from: "samuel.santibout@gmail.com",
+    to: [
+      "santibout@yahoo.com",
+      "santibout01@gmail.com",
+      "samuel.santibout@gmail.com",
+    ],
+    subject: "Portfolio Messeage",
+    html: `<h1>From: ${name}</h1>
+    <h3>${email}</h3>
+    <p>${message}</p>`,
+  };
+  (async () => {
+    try {
+      await sgMail.send(msg);
+    } catch (error) {
+      console.error(error);
+
+      if (error.response) {
+        console.error(error.response.body);
+      }
     }
-  });
-  // console.log("message: ", message);
-  sendMail(name, email, message, function (err, data) {
-    if (err) {
-      res.status(500).json({ message: "self written internal error" });
-    } else {
-      res.json({ message: "self written email sent" });
-    }
-  });
+  })();
 });
 
-app.listen(3201, function () {
-  console.log("Z.E.R.O - 1 Systems Intialized on port 3201");
+app.listen(PORT, function () {
+  console.log(`Z.E.R.O - 1 Systems Intialized on port ${PORT}`);
 });
